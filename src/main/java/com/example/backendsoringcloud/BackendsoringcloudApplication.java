acf0df05281c61f2b2ecc9a50b8f9427ea4a1b6a
Nathan Picchiottino <nathanpicchiottino@gmail.com>
2022-01-03 13:46:57 +0100

    Initial Commit

---
+++ src/main/java/com/example/backendsoringcloud/BackendsoringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsoringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsoringcloudApplication {
+//Test
+    public static void main(String[] args) {
+        SpringApplication.run(BackendsoringcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendsoringcloud/BackendsoringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendsoringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsoringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
