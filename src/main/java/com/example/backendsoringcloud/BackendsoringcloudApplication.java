b0e2e662dcc11888be7f4488a9a387946359387e
Nathan Picchiottino <nathan.picchiottino@efrei.net>
2022-01-03 17:00:06 +0100

    Main file

--- src/main/java/com/example/backendsoringcloud/BackendsoringcloudApplication.java
+++ src/main/java/com/example/backendsoringcloud/BackendsoringcloudApplication.java
@@ -1,13 +1,24 @@
 package com.example.backendsoringcloud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendsoringcloudApplication {
-//Test
+public class BackendsoringcloudApplication implements CommandLineRunner {
+    //TODO Les classes et repositories
+    //TODO Code here
+
+    //@Autowired
+    //    private QuestionnaireRepository questionnaireRepository;
     public static void main(String[] args) {
         SpringApplication.run(BackendsoringcloudApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application is running in console");
+        //TODO Code here
+    }
 }
