0f35a250645cc9189154ba8ca1c048c410551bed
Nathan Picchiottino <nathan.picchiottino@efrei.net>
2022-01-03 17:36:20 +0100

    Sensitivity Rest Controller

---
+++ src/main/java/web/SensitivityRestController.java
@@ -0,0 +1,8 @@
+package web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @RequestMapping("sensitivities")
+public class SensitivityRestController {
+}
