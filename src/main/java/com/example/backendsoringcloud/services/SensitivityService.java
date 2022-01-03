30709d590d76db486b7e65dadfc1a7c88c2958d9
Nathan Picchiottino <nathan.picchiottino@efrei.net>
2022-01-03 18:15:56 +0100

    All methods for Sensitivity

--- src/main/java/com/example/backendsoringcloud/services/SensitivityService.java
+++ src/main/java/com/example/backendsoringcloud/services/SensitivityService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SensitivityService {
@@ -22,5 +23,43 @@ public class SensitivityService {
      */
     public List<Sensitivity> getSensitivities(){
         return sensitivityRepository.findAll();
+    }
+
+    /**
+     * Get a sensitivity by its id
+     * @param id the id of the sensitivity
+     * @return the sensitivity with this id
+     */
+    public Optional<Sensitivity> getSensitivityById(int id){
+        return sensitivityRepository.findById(id);
+    }
+
+    public Sensitivity newSensitivity(String gameName, double sensitivityValue, String resolutionValue) {
+
+        Sensitivity newSensitivity = new Sensitivity(gameName, sensitivityValue, resolutionValue);
+        return sensitivityRepository.save(newSensitivity);
+    }
+
+    public Sensitivity replaceSensitivity(int questionId, String gameName, double sensitivityValue, String resolutionValue) {
+
+        return sensitivityRepository.findById(questionId)
+                .map(sensitivity ->{
+                    sensitivity.setGameName(gameName);
+                    sensitivity.setSensitivityValue(sensitivityValue);
+                    sensitivity.setResolutionValue(resolutionValue);
+                    return sensitivityRepository.save(sensitivity);
+                }).orElseGet(() -> {
+                    Sensitivity newSensitivity = new Sensitivity(gameName, sensitivityValue, resolutionValue);
+                    newSensitivity.setQuestionId(questionId);
+                    return sensitivityRepository.save(newSensitivity);
+                });
+    }
+
+    /**
+     * Delete a sensitivity by its ID
+     * @param id the id of the sensitivity to delete
+     */
+    public void deleteSensitivity(int id) {
+        sensitivityRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/backendsoringcloud/web/SensitivityRestController.java
+++ src/main/java/com/example/backendsoringcloud/web/SensitivityRestController.java
@@ -1,13 +1,11 @@
 package com.example.backendsoringcloud.web;
 
 import com.example.backendsoringcloud.entities.Sensitivity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.backendsoringcloud.services.SensitivityService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController @RequestMapping("sensitivities")
 public class SensitivityRestController {
@@ -26,5 +24,35 @@ public class SensitivityRestController {
     @RequestMapping(method = RequestMethod.GET)
     public List<Sensitivity> getSensitivities(){
         return sensitivityService.getSensitivities();
+    }
+
+    /**
+     * Méthode appelée lorsque l'url est /sensitivities/{id}
+     * @param id l'id de la sensitivities
+     * @return La sensitivities qui porte cet ID
+     */
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Optional<Sensitivity> getSensitivityById(@PathVariable int id){
+        return sensitivityService.getSensitivityById(id);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json", params = {"gameName", "sensitivityValue", "resolutionValue"})
+    Sensitivity newSensitivity(@RequestParam String gameName, @RequestParam double sensitivityValue, @RequestParam String resolutionValue) {
+
+        return sensitivityService.newSensitivity(gameName, sensitivityValue, resolutionValue);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json", params = {"questionId" , "gameName", "sensitivityValue", "resolutionValue"})
+    Sensitivity replaceSensitivity(@RequestParam int questionId, @RequestParam String gameName, @RequestParam double sensitivityValue, @RequestParam String resolutionValue) {
+        return sensitivityService.replaceSensitivity(questionId, gameName, sensitivityValue, resolutionValue);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    void deleteSensitivity(@PathVariable int id) {
+        sensitivityService.deleteSensitivity(id);
     }
 }
